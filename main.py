119e84c9fdf12e5ffc0c6b4e3be59bc7b200920b
knstntn-ts <79147005+knstntn-ts@users.noreply.github.com>
2022-07-19 19:21:42 +0300

    Modify html pages, add comments to code.

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
+##### IMPORT STATEMENTS
 from flask import Flask, jsonify, render_template
 import requests
 import random
 
-app = Flask(__name__)
 
+##### MAIN VARIABLES
+app = Flask(__name__)
+# link for real elephant API
 url_all = 'https://elephant-api.herokuapp.com/elephants'
 
 
+# Home page
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
+# Display info of a random elephant in a JSON format
 @app.route('/random', methods=["POST", "GET"])
 def random_ele_fun():
+    # Request data from the website
     elephants = requests.get(url_all)
     elephants_data = elephants.json()
+
+    # Randomly choose an elephant
     random_elephant = random.choice(elephants_data)
     return jsonify(elephant=random_elephant)
 
 
+# Display info of all available elephants in a JSON format
 @app.route('/all', methods=["POST", "GET"])
-def all_cafes():
+def all_elephants():
     elephants = requests.get(url_all)
     elephants_data = elephants.json()
     return jsonify(elephant=elephants_data)
 
 
+# --- RUN THE API --- #
 if __name__ == '__main__':
     app.run(debug=True)
